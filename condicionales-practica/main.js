96c59770fe7175b7f903fb56ef3c3c0e469b25fa
Alexis Orphant <alexisorphant@gmail.com>
2022-06-16 01:42:18 -0300

    practica de condicionales

---
+++ condicionales-practica/main.js
@@ -0,0 +1,72 @@
+// EJ "1"
+
+// let num1 = parseInt(prompt("ingrese un numero 1"));
+// let num2 = parseInt(prompt("ingrese otro numero 2"));
+
+// if(num1 > num2){
+//     console.log("el numero 1: " + num1 + " es mayor que el numero 2: " + num2);
+// }else if(num2 > num1){
+//     console.log("el numero 2: " + num2 + " es mayor que el numero 1: " + num1);
+// }else{
+//     console.log("los numero 1: " + num1 + " y numero 2: " + num2 + " son iguales")
+// }
+
+//EJ "2"
+
+// let cantidadHelados = parseInt(prompt("ingrese la cantidad de helados que quiere"))
+
+// if(cantidadHelados == 1){
+//     console.log("el costo es de $5")
+// }else if(cantidadHelados == 2){
+//     console.log("el costo es de $10")
+// }else if(cantidadHelados == 3){
+//     console.log("el costo es de $15")
+// }else if(cantidadHelados == 4){
+//     console.log("el costo es de $20")
+// }else if(cantidadHelados == 5){
+//     console.log("el costo es de $25")
+// }else if(cantidadHelados == 6){
+//     console.log("el costo es de $30")
+// }else if(cantidadHelados == 7){
+//     console.log("el costo es de $35")
+// }else if(cantidadHelados == 8){
+//     console.log("el costo es de $40")
+// }else if(cantidadHelados == 9){
+//     console.log("el costo es de $45")
+// }else if(cantidadHelados == 10){
+//     console.log("el costo es de $50")
+// }else{
+//     console.log("el costo es mayor a $50")
+// } 
+// let pagar = 5
+// if(cantidadHelados != ""){
+//     console.log("el precio es: $ " + cantidadHelados * pagar)
+// }else {
+//     console.log("ingrese un valor valido")
+// }
+
+// EJ 3
+
+// let anioNacimiento = parseInt(prompt("ingrese su año de nacimiento"));
+
+// if(anioNacimiento <= 2022){
+//     let edad = 2022 - anioNacimiento;
+//     console.log("su edad es: " + edad)
+// }else{
+//     console.log("el valor no es valido")
+// }
+
+// EJ 5
+
+// let anioNacimiento = parseInt(prompt("ingrese su año de nacimiento"));
+// let edad = 2022 - anioNacimiento;
+
+// if(edad >= 18){
+//     let nombre = prompt("ingrese su nombre");
+//     let apellido = prompt("ingrese su apellido");
+//     alert("hola " + nombre + " " + apellido +", tu edad es: " + edad);
+// }else if(edad <= 17){
+//     alert("usted es menor de edad");
+// }else {
+//     alert("ingrese un valor valido");
+// }
