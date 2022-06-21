b9ee80854449bfe82454663dda2b6cd07106f0af
Alexis Orphant <alexisorphant@gmail.com>
2022-06-20 22:57:00 -0300

    clase 4

--- bucles-practica/main.js
+++ bucles-practica/main.js
@@ -39,7 +39,7 @@
 
 // EJ 3
 
-let numero = parseInt(prompt("ingrese un numero"));
+// let numero = parseInt(prompt("ingrese un numero"));
 
 // for(i = numero; i >= 0; i--){
 //     let resultado = i % 2;
@@ -50,11 +50,69 @@ let numero = parseInt(prompt("ingrese un numero"));
 // }
 // console.log("sali del for")
 
-while(numero >= 0){
-    numero = numero % 2;
-    if(numero !== 0){
-        continue;
-    }
-    console.log(numero)
-}
-console.log("sali del while")
+// EJ 4
+
+// let numeroUno = parseInt(prompt("ingrese un numero"));
+// let numeroDos = parseInt(prompt("ingrese otro numero"));
+// let resultado = 0;
+
+// for(i = 0; i < numeroUno; i++){
+//     resultado = resultado + numeroDos;
+// }
+//  console.log(resultado)
+
+// for(i = 0; i < numeroDos; i++){
+//     resultado += numeroUno;
+// }
+// console.log(resultado);
+
+// EJ 9 ver clase para hacerlo bien
+// let cantidadDeNumeros = parseInt(prompt("ingrese un numero"));
+
+// while(cantidadDeNumeros <=){
+//     cantidadDeNumeros = parseInt(prompt("ingrese un numero"));
+// }
+
+// let mayor = 0;
+// let menor = 0;
+// let media = 0;
+
+// for(i = 1; i <= cantidadDeNumeros; i++){
+//     let numero = parseInt(prompt("ingrese el numero " + i));
+//     //calcular el mayor
+//     if(numero > mayor){
+//         mayor = numero;
+//     }
+//     // calcular el menor
+//     if(numero < menor){
+//         menor = numero;
+//     }
+//     //calcular la media 
+//     media = media + numero;
+// }
+
+// media = media / cantidadDeNumeros;
+// console.log("el mayor es: " + mayor)
+// console.log("el menor es: " + menor)
+// console.log("la media es: " + media)
+
+// EJ 5 
+// let dividendo = parseInt(prompt("ingrese el dividendo"));
+// let divisor = parseInt(prompt("ingrese el divisor"));
+// resultado = 0;
+
+// while(dividendo >= divisor){
+//     dividendo = dividendo - divisor;
+//     resultado += 1;
+// }
+
+// // EJ 8
+// let numero = parseInt(prompt("ingrese un numero"));
+// let resultado = 0;
+
+
+// while(numero > 0){
+//     numero = parseInt(numero / 10);
+//     resultado += 1
+// }
+// console.log(resultado)
---
+++ clase4/main.js
@@ -0,0 +1,72 @@
+// let numero1 = parseInt(prompt("ingrese un numero"));
+// let numero2 = parseInt(prompt("ingrese un numero"));
+
+// function sumar(){
+//     let resultado = numero1 + numero2;
+//     console.log("la suma es: " + resultado);
+// }
+
+// function multiplicar(){
+//     console.log("el resultado de la multiplicacion es: " + (numero1 * numero2));
+// }
+
+// sumar();
+// multiplicar();
+
+// function pedirNumeroYSumarleUno() {
+//     let numero = parseInt(prompt("ingrese un numero"));
+//     alert(numero + 1);
+// }
+// pedirNumeroYSumarleUno();
+
+
+// function sumar() {
+//     const numero2 = 5;
+//     const resultado = numero2 + numero1;
+// }
+
+// let numero1 = parseInt(prompt("ingrese un numero"))
+
+// function multiplicacion(numero1, numero2){
+//     //retorna una variable, y muestra el resultado
+//     const resultado = numero1 * numero2;
+//     return resultado;
+//     //retorna el valor directamente
+//     // return numero1 * numero2
+// }
+
+// let n1 = parseInt(prompt("ingrese un numero"));
+// let n2 = parseInt(prompt("ingrese otro numero"));
+
+// const resultadoMultiplicacion = multiplicacion(n1, n2);
+
+// console.log(resultadoMultiplicacion)
+
+// function calculadora (primerNumero, segundoNumero, operacion){
+//     switch(operacion){
+//         case "+":
+//             return primerNumero + segundoNumero;
+//             break;
+//         case "-":
+//             return primerNumero - segundoNumero;
+//             break;
+//         case "*":
+//             return primerNumero * segundoNumero;
+//             break;
+//         case "/":
+//             return primerNumero / segundoNumero;
+//             break;
+//         default:
+//             return 0;
+//             break;
+//     }
+// }
+// console.log(calculadora);
+
+function calcularEdad(anioDeNaciomiento){
+    return 2022 - anioDeNaciomiento;
+}
+const anioIngresado = parseInt(prompt("ingrese su año de nacimiento"));
+
+console.log(calcularEdad(anioIngresado))
+// min 1:06:00
