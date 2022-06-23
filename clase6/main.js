801917405315f9ff90840b0cd072eb3caf94a610
Alexis Orphant <alexisorphant@gmail.com>
2022-06-22 23:37:33 -0300

    clase6, arrays

--- clase6/main.js
+++ clase6/main.js
@@ -10,3 +10,11 @@
 // const arrayD = [true,false,true,false];
 // // Declaracion de array mixto
 // const arrayE = [1,false,"C4"];
+
+
+// ACCESO AL ARRAY
+const  numeros = [1,2,3,4,5];
+console.log( numeros[0] ) // 1; 
+console.log( numeros[3] ) // 4; 
+let resultado  = numeros[1] + numeros[2] 
+console.log( resultado  ) // 5;
