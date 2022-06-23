ff5cfe79c584723c06543638d6c17882db3d2955
Alexis Orphant <alexisorphant@gmail.com>
2022-06-22 23:32:57 -0300

    clase 6, Arrays

---
+++ clase6/main.js
@@ -0,0 +1,12 @@
+//  ARRAYS 
+
+// // Declaraciòn de array vacío
+// const arrayA = [];
+// // Declaracion de array con nùmeros
+// const arrayB = [1,2];
+// // Declaracion de array con strings
+// const arrayC = ["C1","C2","C3"];
+// // Declaracion de array con booleanos
+// const arrayD = [true,false,true,false];
+// // Declaracion de array mixto
+// const arrayE = [1,false,"C4"];
