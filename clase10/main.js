b7b5ccf9fbc1637e31bf2d76a1e475d88588ecb6
Alexis Orphant <alexisorphant@gmail.com>
2022-07-10 14:19:18 -0300

    clase10, storage y json

--- clase10/main.js
+++ clase10/main.js
@@ -8,7 +8,7 @@
 // clave = nombre para identificar el elemento 
 // valor = valor/contenido del elemento 
 
-// localStorage.setItem('bienvenida', '¡Hola Coder!');
+// localStorage.setItem('bienvenida', '¡Hola Coder!'); // el primero es el nombre de la clave con la que se va a guardar el dato y el segundo es el valor que tiene 
 // localStorage.setItem('esValido', true);
 // localStorage.setItem('unNumero', 20);
 
@@ -64,3 +64,48 @@
 // localStorage.clear()    //elimina toda la información
 // sessionStorage.clear() //elimina toda la información
 
+// hecho en clase
+
+// localStorage.setItem("totalCarrito", 1500); //para setear la key y value
+//sessionStorage, se matiene por ventana de navegador y no por pestaña
+// const totalCarrito = localStorage.getItem("totalCarrito"); //devuelve strings
+
+// console.log(totalCarrito)
+
+// GUARDAR ARRAYS
+// const alumnos = ["Joaquin", "Gonzalo", "Agustina"];
+
+// localStorage.setItem("alumnos", alumnos);
+
+// const alumnos = localStorage.getItem("alumnos");
+
+// const alumnosArray = alumnos.split(","); // paso una "," porque el string que devuelve, separa los nombres con ",". paso el mismo elemento para que el split lo convierta en un array
+
+// console.log(alumnosArray.join(",")); // el join(), hace lo contrario al split
+
+// RECORRER EL STORAGE
+
+// for(let i = 0; i < localStorage.length; i++){
+//     // indices de las pocisiones de las claves
+//     const clave = localStorage.key(i);
+//     const valor = localStorage.getItem(clave);
+//     console.log(`para clave ${clave}, el valor es ${valor}`)
+// };
+
+// ELIMINAR DATOS DEL STORAGE
+
+// localStorage.removeItem("alumnos") // elimina la clave que recibe
+//localStorage.clear(); // elimina todo del storage
+
+
+/*STORAGE INTERACTUANDO CON EL HTML */
+
+const formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    localStorage.setItem("formularioEnviado", 1);
+
+    // min 33:17
+})
